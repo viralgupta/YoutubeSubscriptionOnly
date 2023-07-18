0ff81753a7872a276afcffa13196ecd752a5179d
viralgupta <guptaviral0@gmail.com>
2023-07-18 06:17:45 +0530

    fixed bugs

--- pages/api/deletecookie.js
+++ pages/api/deletecookie.js
@@ -1,9 +1,17 @@
-import { deleteCookie, getCookies } from 'cookies-next';
+import { setCookie, getCookies } from 'cookies-next';
 
 export default function handler(req, res) {
     const cookies = getCookies({ req, res})
     Object.keys(cookies).forEach(key => {
-      deleteCookie(key, { req, res })
+      setCookie(key, "Deleting", { req, res,
+        maxAge: 5, // 1 year
+        httpOnly: false,
+        domain: process.env.DOMAIN,
+        path: "/",
+        sameSite: "lax",
+        secure: false,
+        expires: new Date(0)
+    });
     });
     res.status(200).json({success: true})
 }
